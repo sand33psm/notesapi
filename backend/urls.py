5a0152a8812114429f5aef64b3e08a75e405deb8
sndp <sand33psm@gmail.com>
2024-11-09 00:04:57 +0530

    update url routing to handle page refresh

--- backend/urls.py
+++ backend/urls.py
@@ -6,6 +6,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
+    re_path(r'^(?!api/).*$', TemplateView.as_view(template_name="index.html")),
 
 ]
